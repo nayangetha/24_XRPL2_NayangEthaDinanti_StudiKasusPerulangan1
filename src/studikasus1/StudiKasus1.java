761341b75ab313b33a7f6a2318fa12243af48b23
unknown <nayang_etha_29rpl@student.smktelkom-mlg.sch.id>
2020-11-13 13:38:43 +0700

    first commit

---
+++ src/studikasus1/StudiKasus1.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studikasus1;
+import java.util.Scanner;
+/**
+ *
+ * @author MOKLET1
+ */
+public class StudiKasus1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        
+        System.out.println("Menghitung Huruf 'a' Pada Suatu Nama");
+        System.out.println("____________________________________");
+        Scanner a = new Scanner(System.in);
+        
+        System.out.println("Masukkan Nama Anda :");
+        
+        //Deklarasi variable
+        String nama = a.nextLine();
+        char huruf = 'a';
+        int jumlah=0;
+       
+        //looping ke setiap huruf di variabel nama
+        for(int i = 0; i<nama.length(); i++){
+           
+        //memastikan huruf ke-i = huruf yang dicari
+        if(nama.charAt(i)== huruf){
+        
+            //jika iya maka bertambah 1
+            jumlah++;
+               
+           }
+       }
+       
+       //hasil perhitungan
+        System.out.println("Jumlah huruf '"+huruf+"' pada nama "+nama+ " adalah " + jumlah);
+                    
+                
+                }
+            }
+        
+        
+    
+
+    
+
+
+        
+ 
+   
+
